24da2e56f8e5e68dd7a2e3597c8d141a8355fcb0
HUSKI3 <doslunar8@gmail.com>
2023-06-15 22:18:03 +0200

    Dumb mistake, remove redundant if

--- src/dollconfig/config.v
+++ src/dollconfig/config.v
@@ -19,7 +19,7 @@ struct BaseConfigFile {
 }
 
 pub fn (mut config Config) add_pulled(pulled string) {
-	if !(pulled in config.pulled_files) {
+	if pulled !in config.pulled_files {
 		config.pulled_files << pulled
 	} else {
 		println("Didn't add pulled file to config, it already exists")
@@ -27,10 +27,7 @@ pub fn (mut config Config) add_pulled(pulled string) {
 }
 
 pub fn (mut config Config) pulled_exists(pulled string) bool {
-	if pulled in config.pulled_files {
-		return true
-	}
-	return false
+	return pulled in config.pulled_files
 }
 
 pub fn (config Config) save_config() {
@@ -38,36 +35,33 @@ pub fn (config Config) save_config() {
 
 	out_conf := json.encode(config)
 
-	/* 
+	/*
 	* We recreate the config every time.
-	* This is just simpler than dealing with the 
+	* This is just simpler than dealing with the
 	* buffer not being cleaned properly
 	* Why? idfk, ask C
 	*/
 	mut output := os.create(conf_path) or {
-    	eprintln("Failed to recreate config")
+		eprintln('Failed to recreate config')
 		exit(1)
 	}
 
-	output.write(out_conf.bytes()) or {
-		panic("[Panic] ${err} to ${conf_path}")
-	}
+	output.write(out_conf.bytes()) or { panic('[Panic] ${err} to ${conf_path}') }
 }
-
 
 pub fn load_config() Config {
 	conf_path := os.getwd() + '/config.doll'
 
 	source := os.read_file(conf_path) or {
-		eprintln("Directory is not a project")
+		eprintln('Directory is not a project')
 		exit(1)
 	}
 
 	config := json.decode(BaseConfigFile, source) or {
-		panic("Invalid config supplied. It seems to be corrupt?")
+		panic('Invalid config supplied. It seems to be corrupt?')
 	}
 
-	return Config {
+	return Config{
 		project_path: os.getwd()
 		config_path: conf_path
 		pulled_files: config.pulled_files
